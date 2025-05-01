264c3d42b2e0b63f052d0ea23f2cbf9fb3703bec
yaninaspina1 <yanina_05_19@hotmail.com>
2025-05-01 09:20:59 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -91,14 +91,17 @@ BUSINESS_ID_EL_CAMINO_REAL = "julsvvavzvghwffkkm0nlg"
 
 
 
-def show_competencia():
- 
-opcion = option_menu("Navegación", 
-    ["Inicio", "Explorar Reseñas y KPIs", "Análisis Integral de Competencia"],
-    icons=["house", "bar-chart", "graph-up-arrow"],
-    menu_icon="cast",
-    default_index=0,
-)
+ # --- SIDEBAR ---
+with st.sidebar:
+    opcion = option_menu(
+        "Navegación", 
+        ["Inicio", "Explorar Reseñas y KPIs", "Análisis Integral de Competencia"],
+        icons=["house", "bar-chart", "graph-up-arrow"],
+        menu_icon="cast",
+        default_index=0
+    )
+
+
 # --- INICIO ---
 
 if opcion == "Inicio":
